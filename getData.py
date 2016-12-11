5b0a5c52c771632b59e9d7cfd2f606ec1dc9168f
Abi-Abhisek <www.pikun@gmail.com>
2016-12-12 02:34:47 +0530

    Thrash Cleared

--- getData.py
+++
@@ -1,8 +0,0 @@
-from getTime import getTime
-from getTemp import getTemp
-
-while True:
-        dtime = getTime()
-        temp = getTemp()
-        with open("data.tsv","a") as myfile:
-                myfile.write(dtime  +"\t" +str(temp) +"\n")                                                               
