42dcfc67f111222d09ff9c37573774b88498e7d1
Abi-Abhisek <www.pikun@gmail.com>
2016-12-12 02:32:14 +0530

    Graph Update and Routing

--- index.py
+++ index.py
@@ -1,11 +1,38 @@
 from flask import request, Flask, url_for
-from flask_socketio import emit,SocketIO
+from threading import Timer
 
 app = Flask(__name__)
 
-@app.route('/data')
+def getTemp():
+    import time
+    import subprocess
+    temp_address = "/sys/bus/w1/devices/28-03160140daff/w1_slave"
+    s = 1
+    while s==1:
+        file = open(temp_address, "r")
+        data = file.read()
+        file.close()
+        a = data.split()
+        b = a[len(a) - 1]
+        reading = b[2:]
+        temp = float(reading) / 1000
+      	s = s + 1
+        return temp
+
+def getTime():
+	import time
+	data = time.ctime()
+	raw = data.split(" ")[3]
+	return raw
+
 def data() :
-	fo = open("data.txt","r+")
-	raw_data = fo.read(10);
-	fo.close()
-	return raw_data
+	dtime = getTime()
+    temp = getTemp()
+    with open("/Views/Data/data.tsv","a") as myfile:
+         myfile.write(dtime+"\t" +str(temp) +"\n")                                                               
+
+
+@app.route('/data')
+def work():
+	t = Timer(1.00,data);
+	t.start();
