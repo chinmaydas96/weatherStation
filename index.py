b8251d38233dc49201e1582e64b96579662fc2c8
chinmaydas96 <chinmaydasbat@gmail.com>
2016-12-12 02:42:22 +0530

    identation error fix

--- index.py
+++ index.py
@@ -3,12 +3,13 @@ from threading import Timer
 
 app = Flask(__name__)
 
+
 def getTemp():
     import time
     import subprocess
     temp_address = "/sys/bus/w1/devices/28-03160140daff/w1_slave"
     s = 1
-    while s==1:
+    while s == 1:
         file = open(temp_address, "r")
         data = file.read()
         file.close()
@@ -16,23 +17,25 @@ def getTemp():
         b = a[len(a) - 1]
         reading = b[2:]
         temp = float(reading) / 1000
-      	s = s + 1
+        s = s + 1
         return temp
 
-def getTime():
-	import time
-	data = time.ctime()
-	raw = data.split(" ")[3]
-	return raw
 
-def data() :
-	dtime = getTime()
+def getTime():
+    import time
+    data = time.ctime()
+    raw = data.split(" ")[3]
+    return raw
+
+
+def data():
+    dtime = getTime()
     temp = getTemp()
-    with open("/Views/Data/data.tsv","a") as myfile:
-         myfile.write(dtime+"\t" +str(temp) +"\n")                                                               
+    with open("/Views/Data/data.tsv", "a") as myfile:
+        myfile.write(dtime + "\t" + str(temp) + "\n")
 
 
 @app.route('/data')
 def work():
-	t = Timer(1.00,data);
-	t.start();
+    t = Timer(1.00, data)
+    t.start()
