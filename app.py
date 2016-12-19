2ea59e943515f0c04b863d3fbd9bf7b0e79e119a
Abi-Abhisek <www.pikun@gmail.com>
2016-12-19 20:10:21 +0530

    Necessary JS change

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from threading import Timer
 
 app = Flask(__name__)
 
-
+"""
 def getTemp():
     temp_address = "/sys/bus/w1/devices/28-03160140daff/w1_slave"
     s = 1
@@ -32,6 +32,7 @@ def data():
     with open("/static/data.tsv", "a") as myfile:
         myfile.write(dtime + "\t" + str(temp) + "\n")
     threading.Timer(1.0, data).start()
+"""
 
 @app.route('/')
 def work():
@@ -39,4 +40,4 @@ def work():
     data()
     
 if __name__ == '__main__':
-    app.run(host='192.168.43.228')
+    app.run()
